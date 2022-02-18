41b8bec9172cf36d6e37106fd44c2f666db92011
Abu <abubakarabukar@gmail.com>
2022-02-18 01:44:05 -0500

    Added event card and started event catalog

---
+++ src/Components/EventCatalog/EventCatalog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventCatalog = () => {
+  return (
+    <div>EventCatalog</div>
+  )
+}
+
+export default EventCatalog
--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -4,19 +4,37 @@ import InputField from "../Components/UI/InputField/InputField";
 import ModalService from "../Services/ModalService";
 import ModalRoot from "../Components/UI/Modal/ModalRoot";
 import ConfirmModal from "../Components/ConfirmModal/ConfirmModal";
+import EventCard from "../Components/EventCard/EventCard";
+import logo from "../Assets/Images/logo.svg";
+import fans from "../Assets/Images/fans_cheering.jpeg";
 function Events() {
-
   const addModal = () => {
     ModalService.open(ConfirmModal);
   };
-
+  const events = [
+    {
+      img: logo,
+      title: "Superb Owl",
+      location: "Yum Center",
+      pricesFrom: 99,
+    },
+    {
+      img: fans,
+      title: "Event",
+      location: "Place",
+      pricesFrom: 49,
+    },
+  ];
   return (
     <div>
-      <div> 
-      Hello World
-      </div>
+      <div>Hello World</div>
       <div>
-        <Button onClick={() => {console.log("Click");}} type={Button} >
+        <Button
+          onClick={() => {
+            console.log("Click");
+          }}
+          type={Button}
+        >
           Click Me!
         </Button>
         <InputField
@@ -28,7 +46,18 @@ function Events() {
       </div>
       <ModalRoot />
       <Button onClick={addModal}>Open Modal</Button>
-
+      <EventCard
+        img={events[0].img}
+        title={events[0].title}
+        location={events[0].location}
+        pricesFrom={events[0].pricesFrom}
+      ></EventCard>
+      <EventCard
+        img={events[1].img}
+        title={events[1].title}
+        location={events[1].location}
+        pricesFrom={events[1].pricesFrom}
+      ></EventCard>
     </div>
   );
 }
