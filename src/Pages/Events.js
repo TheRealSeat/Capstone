fb691e5270547277bdf497e6b0c264f5e71136a8
Abu <abubakarabukar@gmail.com>
2022-02-21 17:41:09 -0500

    Seperated catalog data in data folder

--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -1,43 +1,13 @@
 import React, { useState } from "react";
 import Button from "../Components/UI/Button/Button";
-import logo from "../Assets/Images/logo.svg";
-import fans from "../Assets/Images/fans_cheering.jpeg";
 import ConfirmModal from "../Components/ConfirmModal/ConfirmModal";
 import ProductTable from "../Components/ProductTable/ProductTable";
+import { catalogs } from "../Data/catalogsData";
+
 // import Navbar from "../Components/Navbar";
 function Events() {
-  const genericEvent1 = {
-    img: logo,
-    title: "Superb Owl",
-    location: "Yum Center",
-    pricesFrom: 99,
-  };
 
-  const genericEvent2 = {
-    img: fans,
-    title: "Event",
-    location: "Place",
-    pricesFrom: 49,
-  };
 
-  const catalogs = [
-    {
-      catalogName: "Sports",
-      events: [genericEvent1, genericEvent2, genericEvent1, genericEvent2],
-    },
-    {
-      catalogName: "Concerts",
-      events: [genericEvent2, genericEvent2, genericEvent2],
-    },
-    {
-      catalogName: "Family",
-      events: [genericEvent1, genericEvent2, genericEvent2, genericEvent2],
-    },
-    {
-      catalogName: "Comedy",
-      events: [genericEvent1, genericEvent2, genericEvent2],
-    },
-  ];
 
   const [showModal, setModalState] = useState(false);
   const [modalProps, setModalProps] = useState({
