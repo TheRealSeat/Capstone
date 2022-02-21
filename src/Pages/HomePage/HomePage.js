08947fde3a4bafed7e09bd87561719b7f0b3c6f1
Lnguyen1996 <97846628+Lnguyen1996@users.noreply.github.com>
2022-02-21 13:27:25 -0500

    Revert "Revert "clean up route extra tag""
    This reverts commit b9a12f6d924affa7c89bb4a2628f76c99efd4f83.

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {
 import 
 {
   Button,
@@ -9,7 +8,6 @@ import
   Icon,
   Label,
   Segment,
-} from "semantic-ui-react";
   Image,
   Header,
   Divider,
@@ -33,7 +31,6 @@ const HomePage = () => {
               <Grid columns="2">
                 <Grid.Row>
                   <Grid.Column floated textAlign="right">
-                    <div className="white-large">Where your seat is placed</div>
                     <div className="white-huge">Where your seat is placed</div>
                   </Grid.Column>
                   <Grid.Column textAlign="left">
@@ -172,13 +169,6 @@ const HomePage = () => {
                 <div className="orange-large"> TheRealSeat </div>
 
                 <div className="dark-large" style={{ marginLeft: "20px" }}>
-                  Today
-                </div>
-              </h1>
-              <div>
-                <p style={{fontSize:'20px',marginBottom:'15px'}}>
-                Sign up to receive our latest updates and developments. Plus get information about early access and upcoming events.                </p>
-              </div>
                   Today{" "}
                 </div>
               </h1>
@@ -186,12 +176,6 @@ const HomePage = () => {
                 <Grid columns="1">
                   <Grid.Row>
                     <Form.Field>
-                      <input placeholder="Your Name" style={{height:'50px',backgroundColor:'GrayText',width:'60%'}} />
-                    </Form.Field>
-                  </Grid.Row>
-                  <Grid.Row style={{marginTop:'10px'}}>
-                    <Form.Field>
-                      <input placeholder="Your Email"  style={{height:'50px',backgroundColor:'GrayText',width:'60%'}} />
                       <input placeholder="Name" />
                     </Form.Field>
                   </Grid.Row>
@@ -202,7 +186,6 @@ const HomePage = () => {
                   </Grid.Row>
                   <Grid.Row>
                     <Form.Field>
-                      <Button type="submit" color="grey" size="massive">Join Now</Button>
                       <Button type="submit">Submit</Button>
                     </Form.Field>
                   </Grid.Row>
