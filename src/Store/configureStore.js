68934a85f04976b9555c71e8ed00cef0341f03ab
Lnguyen1996 <97846628+Lnguyen1996@users.noreply.github.com>
2022-03-02 19:19:04 -0500

    redux implement
    Adding redux, and setting up rootReducer  and also adding ReduxDevTool

---
+++ src/Store/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore,applyMiddleware } from "redux"
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from "./rootReducer";
+import thunk from 'redux-thunk';
+export default function configureStore(){
+   const store= createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)))
+   
+
+   return store;
+}
