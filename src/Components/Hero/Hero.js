34bde68cec797c7e5b7a630efc59e1e286e0b1f7
DoctorGo <52477913+DoctorGo@users.noreply.github.com>
2022-03-07 09:13:02 -0500

    updated hero to show full imgs

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -6,44 +6,20 @@ function Hero() {
     return (
     <div className="Hero">
         <Carousel class='carousel'>
-        <Carousel.Item class='carousel-item'>
-            <EventCard
-            class="carousel-card"
-            img="https://images.pexels.com/photos/207691/pexels-photo-207691.jpeg?cs=srgb&dl=pexels-pixabay-207691.jpg&fm=jpg"
-            title="Welcome to TheRealCapstone"
-            location="J.B. Speed School"
-            pricesFrom="0"
-            key="0"
-            handleClick='0'
-            ></EventCard>
+        <Carousel.Item class='carousel-item'>          
+            <img class="hero-img"src='https://images.pexels.com/photos/207691/pexels-photo-207691.jpeg?cs=srgb&dl=pexels-pixabay-207691.jpg&fm=jpg' />
             <Carousel.Caption>
             <h3>Welcome to TheRealCapstone</h3>
             </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-        <EventCard
-            class="carousel-card"
-            img="https://images.pexels.com/photos/11032963/pexels-photo-11032963.jpeg?cs=srgb&dl=pexels-josh-hild-11032963.jpg&fm=jpg"
-            title="Louisville Concerts"
-            location="Louisville"
-            pricesFrom="70"
-            key="1"
-            handleClick='0'
-            ></EventCard>
+        <Carousel.Item class='carousel-item'>       
+            <img class="hero-img" src="https://images.pexels.com/photos/11032963/pexels-photo-11032963.jpeg?cs=srgb&dl=pexels-josh-hild-11032963.jpg&fm=jpg"/>            
             <Carousel.Caption>
             <h3>Louisville Concerts</h3>
             </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-        <EventCard
-            class="carousel-card"
-            img="https://images.pexels.com/photos/3722923/pexels-photo-3722923.jpeg?cs=srgb&dl=pexels-chait-goli-3722923.jpg&fm=jpg"
-            title="Nashville Concerts"
-            location="Nashville"
-            pricesFrom="200"
-            key="0"
-            handleClick='0'
-            ></EventCard>
+        <Carousel.Item class='carousel-item'>      
+            <img class="hero-img" src="https://images.pexels.com/photos/3722923/pexels-photo-3722923.jpeg?cs=srgb&dl=pexels-chait-goli-3722923.jpg&fm=jpg"/>           
             <Carousel.Caption>
             <h3>Nashville Concerts</h3>
             </Carousel.Caption>
