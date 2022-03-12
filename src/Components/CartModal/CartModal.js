e445bed161a0f3c3c4c1d32a1533c15993293fb1
Lnguyen1996 <97846628+Lnguyen1996@users.noreply.github.com>
2022-03-12 12:48:51 -0500

    move cart button to navbar
    change navbar component to function component base and improve UI

--- src/Components/CartModal/CartModal.js
+++ src/Components/CartModal/CartModal.js
@@ -1,8 +1,9 @@
 import Modal from "react-bootstrap/Modal";
 import "../ConfirmModal/ConfirmModal.scss";
 import { useSelector, useDispatch } from "react-redux";
+import { Typography } from "@mui/material";
 import { removeFromCart } from "../../Services/Slices/cart";
-import { Button, Icon } from "semantic-ui-react";
+import { Button, Divider, Icon } from "semantic-ui-react";
 import { Table } from "react-bootstrap";
 const CartModal = ({ show, handleClose }) => {
   const cart = useSelector((state) => state.cart);
@@ -21,43 +22,51 @@ const CartModal = ({ show, handleClose }) => {
         </Modal.Header>
         <Modal.Body style={{ color: "#bac3d6" }}>
           {cart.cartEvents.length > 0 ? (
-            <Table striped bordered hover variant="dark">
-              <thead>
-                <tr>
-                  <th>Catalog</th>
-                  <th>Title</th>
-                  <th>Location</th>
-                  <th>Price</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cart.cartEvents?.map((product, index) => (
+            <>
+              <Table striped bordered hover variant="dark">
+                <thead>
                   <tr>
-                    <td>{product.catalog}</td>
-                    <td>{product.event_title}</td>
-                    <td>{product.event_location}</td>
-                    <td>{product.event_pricesFrom}</td>
-                    <td>
-                      <Button
-                        animated="vertical"
-                        onClick={() => dispatch(removeFromCart(index))}
-                      >
-                        <Button.Content hidden>Delete</Button.Content>
-                        <Button.Content visible>
-                          <Icon name="trash" />
-                        </Button.Content>
-                      </Button>
-                    </td>
+                    <th>Catalog</th>
+                    <th>Title</th>
+                    <th>Location</th>
+                    <th>Price</th>
+                    <th>Action</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
+                </thead>
+                <tbody>
+                  {cart.cartEvents?.map((product, index) => (
+                    <tr>
+                      <td>{product.catalog}</td>
+                      <td>{product.event_title}</td>
+                      <td>{product.event_location}</td>
+                      <td>{product.event_pricesFrom}</td>
+                      <td>
+                        <Button
+                          animated="vertical"
+                          onClick={() => dispatch(removeFromCart(index))}
+                        >
+                          <Button.Content hidden>Delete</Button.Content>
+                          <Button.Content visible>
+                            <Icon name="trash" />
+                          </Button.Content>
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+              <Divider></Divider>
+
+              <Typography
+                variant="h4"
+                sx={{ textAlign: "right", marginRight: "10%" }}
+              >
+                <li>Total: ${cart.cartTotalAmount}</li>
+              </Typography>
+            </>
           ) : (
             <h3>Your cart is empty</h3>
           )}
-
-          <p>Total: ${cart.cartTotalAmount}</p>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={() => handleClose(false)} color="red">
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,48 +1,61 @@
-import React, { Component } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
-import { NavLink} from 'react-router-dom'
-export default class Navbar extends Component {
-  state = { activeItem: 'home' }
+import React, { useState } from "react";
+import { Menu, Segment } from "semantic-ui-react";
+import {  NavLink } from "react-router-dom";
+import CartModal from "../CartModal/CartModal";
+import { useSelector } from "react-redux";
+import { Button } from "semantic-ui-react";
+import { Badge } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+const Navbar = () => {
+  const [activeItem, setActiveItem] = useState("");
+  const [showCart, setCartState] = useState(false);
+  const cart = useSelector((state) => state.cart);
 
-  render() {
-    const { activeItem } = this.state
-
-    return (
-      <Segment inverted>
-        <Menu inverted pointing secondary>
-        <NavLink to='/'>
+  return (
+    <Segment inverted>
+      <Menu inverted pointing secondary>
+        <NavLink to="/">
           <Menu.Item
-            name='Home'
-            active={activeItem === 'Home'}
-            onClick={this.handleItemClick}
+            name="Home"
+            active={activeItem === "Home"}
+            onClick={() => setActiveItem("Home")}
           />
         </NavLink>
-        <NavLink to='/Events'>
+        <NavLink to="/Events">
           <Menu.Item
-            name='Events'
-            active={activeItem === 'Events'}
-            onClick={this.handleItemClick}
+            name="Events"
+            active={activeItem === "Events"}
+            onClick={() => setActiveItem("Events")}
           />
         </NavLink>
-        <NavLink to='/'>
+        <NavLink to="/">
           <Menu.Item
-            name='Fans'
-            active={activeItem === 'Fans'}
-            onClick={this.handleItemClick}
+            name="Fans"
+            active={activeItem === "Fans"}
+            onClick={() => setActiveItem("Fans")}
           />
         </NavLink>
-        <NavLink to='/'>
+        <NavLink to="/">
           <Menu.Item
-            name='Organizers'
-            active={activeItem === 'Organizers'}
-            onClick={this.handleItemClick}
+            name="Organizers"
+            active={activeItem === "Organizers"}
+            onClick={() => setActiveItem("Organizers")}
           />
         </NavLink>
-        </Menu>
-      </Segment>
-    )
-  }
-}
+          <Button style={{backgroundColor:'#576278',marginLeft:'75%',display:'flex'}} onClick={()=>setCartState(true)}>
+          <Badge badgeContent={cart.cartEvents.length} color="secondary">
+              <ShoppingCart />
+            </Badge>
+           <p style={{marginLeft:'5px',color:'#c1c9d9'}}>View Cart</p>
+          </Button>
+           
+         
+      </Menu>
 
+      <CartModal show={showCart} handleClose={() => setCartState(false)} />
+    </Segment>
+  );
+};
+
+export default Navbar;
