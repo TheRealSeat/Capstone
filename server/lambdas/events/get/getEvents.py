e66e4245dca9fadef836fe487b63fda10983f158
Abu <abubakarabukar@gmail.com>
2022-03-22 00:07:44 -0400

    Updated Events lamda: Folder and naming

---
+++ server/lambdas/events/get/getEvents.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+from boto3.dynamodb.connections import Key
+
+client = boto3.client('dynamodb')
+
+def getEvents(event, context):
+    global client
+    response_value = {
+        'statusCode': 500,
+        'body': json.dumps({"error": "Internal Error"}),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    try:
+        data = client.scan(TableName="Events")
+        response_value = {
+            'statusCode': 200,
+            'body': json.dumps(data['Items']),
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+    except Exception as e:
+        print(e)
+
+    return response_value
